c134541f83ac054a1f9604fbee49d626ca9036ce
Tracychen89 <xinchen1215@gmail.com>
2016-02-03 11:28:33 -0800

    Comment all

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace Stack
         // entrance of the class
         static void Main(string[] args)
         {
+            // creat a StackClass object named myStack
             StackClass myStack = new StackClass();
+           // use while loop to show users some information about the stack and some choices for them to choose
             while (true)
             {
                 // clear the console
@@ -23,28 +25,31 @@ namespace Stack
                 Console.WriteLine("4.Display stack element.");
                 Console.WriteLine("5. Exit");
                 Console.Write("Select your choice: ");
+            //int choice accept value from user
                 int choice =
                     Convert.ToInt32(Console.ReadLine());
+                // using switch statement to deal with different choice. The expression in switch should be integral or enumerated type
                 switch (choice)
                 {
                     case 1:
+                        // use Push method in StackClass class
                         myStack.Push(Console.ReadLine());
                         break;
-
+                        // use Peek method in StackClass class
                     case 2:
                         Console.WriteLine("Top element is: {0}", myStack.Peek());
                         break;
-                        
+                        // use Pop method in StackClass class
                     case 3:
                         Console.WriteLine("Element removed: {0}", myStack.Pop());
                         break;
-
+                        // use Display method in StackClass class
                     case 4:
                         myStack.Display();
                         break;
-
+                        // use Environment.Exit method to terminate process 
                     case 5:
-                        System.Environment.Exit(1);
+                        System.Environment.Exit(0);
                         break;
 
 
@@ -55,6 +60,8 @@ namespace Stack
 
         }
     }
+
+    // creat a interface 
     interface StackADT
     {
         Boolean isEmpty();
@@ -63,32 +70,38 @@ namespace Stack
         Object Peek();
         void Display();
     }
-
+    // creat a derived class StackClass based on base class StackADT
     class StackClass : StackADT
     {
+        // I don't understand get and set accessor.
         private int StackSize;
-
         public int StackSizeSet
         {
             get { return StackSize; }
             set { StackSize = value; }
 
         }
+        // defining a int data type named top 
         public int top;
+        // defining a object array named item
         Object[] item;
-
+        // default constructor StackClass as it takes no parameter. 
+        //Default constructor is invoked whenever an object is instantiated by new opertor and no argument provide to new. 
         public StackClass()
         {
             StackSizeSet = 10;
             item = new Object[StackSizeSet];
             top = -1;
         }
+
+        // creat constructor StackClass takes int capacity as argument. Default construtor is invoked when new operator is used.
         public StackClass(int capacity)
         {
             StackSizeSet = capacity;
             item = new Object[StackSizeSet];
             top = -1;
         }
+        // creat a public method named isEmpty and return bool data taype output.
 
         public bool isEmpty()
         {
@@ -96,6 +109,7 @@ namespace Stack
 
             return false;
         }
+        // creat  a public method named Push and takes object element as argument. no output.
         public void Push(object element)
         {
             if (top == (StackSize - 1))
@@ -110,6 +124,7 @@ namespace Stack
             
             }
         }
+        // creat a method named Pop and output is object data type
         public object Pop()
         {
             if (isEmpty())
@@ -122,6 +137,7 @@ namespace Stack
                 return item[top--];
             }
         }
+        // creat a method named Peek and output is object data type
 
         public object Peek()
         {
@@ -136,6 +152,7 @@ namespace Stack
             }
 
         }
+        // creat a method named Display  and no output.
         public void Display()
         {
             for (int i = top; i > -1; i--)
